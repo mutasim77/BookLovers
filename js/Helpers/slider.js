c1efdc132f1ceab70bdaf50b41763a6d8581c5a1
Mutasim <qmutasim77@gmail.com>
2022-12-07 15:46:55 +0600

    Add all Files

---
+++ js/Helpers/slider.js
@@ -0,0 +1,39 @@
+export function slider(event) {
+    // event.target.style.backgroundColor = '#ccc';
+    document.querySelectorAll('.dot').forEach(i => i.style.backgroundColor = '#ccc');
+    let data = (event.target).getAttribute('data-set');
+    let section = $('section.slider');
+    if (data == 1) {
+        $("section.slider h1").css({ "opacity": '0' });
+        $('section.slider p').css({ "opacity": '0' });
+        $('section.slider button').css({ "opacity": '0' });
+        setTimeout(() => {
+            event.target.style.backgroundColor = '#f65d4e';
+            $("section.slider h1").animate({ opacity: '1' }, 'slow');
+            $("section.slider p").animate({ opacity: '1' }, 'slow');
+            $("section.slider button").animate({ opacity: '1' }, 'slow');
+            section.css('background', 'url("http://127.0.0.1:5500/img/slider1.png")')
+            section.css('backgroundSize', 'cover');
+        }, 1000)
+        setTimeout(() => {
+            $('section.slider h1').html('Christmas Pop Up Book Gift Ideas');
+            $('section.slider p').html('Find the perfect gift for everyone on your list');
+        }, 1000)
+    } else {
+        $("section.slider h1").css({ "opacity": '0' });
+        $('section.slider p').css({ "opacity": '0' });
+        $('section.slider button').css({ "opacity": '0' });
+        setTimeout(() => {
+            event.target.style.backgroundColor = '#f65d4e';
+            $("section.slider h1").animate({ opacity: '1' }, 'slow');
+            $("section.slider p").animate({ opacity: '1' }, 'slow');
+            $("section.slider button").animate({ opacity: '1' }, 'slow');
+            section.css('background', 'url("http://127.0.0.1:5500/img/slider2.png")')
+            section.css('backgroundSize', 'cover');
+        }, 1000)
+        setTimeout(() => {
+            $('section.slider h1').html('Reading is for <br> all Ages');
+            $('section.slider p').html('Buy your Books in a Store');
+        }, 1000)
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,27 @@
+'use strict';
+import { slider } from "./Helpers/slider.js";
+
+//! Change Sliders By Clicking 
+let btn = document.querySelectorAll('.dot');
+btn.forEach(item => {
+    item.addEventListener('click', slider.bind(event));
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+AOS.init();
