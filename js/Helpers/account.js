2ed74acd1345f38644a7bbd08d9b1091e94190f9
Mutasim <qmutasim77@gmail.com>
2022-12-19 20:43:04 +0600

    Finsihed'

--- js/Helpers/account.js
+++ js/Helpers/account.js
@@ -1,9 +1,11 @@
+//! login page
 $('#have-account').on('click', () => {
     $('#register').addClass('hide');
     $('#login').removeClass('hide');
     $('.error').addClass('hide');
 });
 
+//! registration page
 $('#create-account').on('click', () => {
     $('#register').removeClass('hide');
     $('#login').addClass('hide');
@@ -12,18 +14,18 @@ $('#create-account').on('click', () => {
 
 //! registration 
 document.querySelector('#log-out').addEventListener('click', () => {
-    let regUsername = $('#reg-username').val();
-    let regEmail = $('#reg-email').val();
-    let regPass = $('#reg-password').val();
-
-    if (!regPass || !regEmail || !regUsername) {
+    let regUsername = $('#reg-username').css('borderColor');
+    let regEmail = $('#reg-email').css('borderColor');
+    let regPass = $('#reg-password').css('borderColor');
+    if (regPass == 'rgb(228, 41, 41)' || regEmail == 'rgb(228, 41, 41)' || regUsername == 'rgb(228, 41, 41)') {
         $('.error').removeClass('hide');
     } else {
+        successRegistare();
         $('.error').addClass('hide');
-
+        $('#register').addClass('hide');
+        $('#login').removeClass('hide');
         //backend
     }
-
 });
 
 //! login in
@@ -36,8 +38,24 @@ document.querySelector('#log-in').addEventListener('click', () => {
     } else {
         $('.error').addClass('hide');
         $('.login').addClass('hide');
-
         $('.success').removeClass('hide');
     }
 
 });
+
+
+//! sucessfully registration 
+function successRegistare() {
+    swal({
+        title: `${$('#reg-username').val()}`,
+        text: `You registered successfully 🤞🏻`,
+        icon: "success",
+    });
+}
+
+
+
+
+//! save to localstorage 
+// localStorage.setItem();
+// localStorage.getItem();
--- js/script.js
+++ js/script.js
@@ -22,3 +22,27 @@ timer();
 AOS.init({
     duration: 700
 });
+
+
+//! changing prifile image
+window.onload = getLocalStorage();
+
+localStorage.setItem('user', 'Nasim');
+// localStorage.clear();
+//!localStorage
+function getLocalStorage() {
+    // return localStorage.getItem('user');
+
+    let storage = localStorage.getItem('user');
+    if (storage) {
+        $('header .header-right > a:first-child').html('<img src="https://static.vecteezy.com/system/resources/previews/006/951/976/non_2x/cute-penguin-cartoon-working-in-front-of-a-laptop-animal-technology-icon-illustration-concept-premium-vector.jpg" style="width: 50px; border-radius: 50px;" alt="picture">');
+        $('section.log-in-out #login').addClass('hide');
+        $('section.log-in-out #register').addClass('hide');
+        $('section.success').removeClass('hide');
+    } else {
+        console.log('No');
+    }
+
+}
+
+
